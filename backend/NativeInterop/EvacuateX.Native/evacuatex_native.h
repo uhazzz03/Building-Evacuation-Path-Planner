cd3c8d32e6062215c3ab3a6573eaf5c6bc07b3f9
Uhass Medan Jayaweera <medanjayaweera@gmail.com>
2026-04-24 01:16:21 +1000

    Added multiple exit function decalration

--- backend/NativeInterop/EvacuateX.Native/evacuatex_native.h
+++ backend/NativeInterop/EvacuateX.Native/evacuatex_native.h
@@ -12,3 +12,5 @@ struct EXPoint { int32_t x; int32_t y; };
 EXTERN int32_t ex_find_path_stub(EXPoint start, EXPoint goal, EXPoint* outPath, int32_t maxPathLen);
 
 EXTERN int32_t ex_find_path_grid(const uint8_t* grid, int32_t width, int32_t height, EXPoint start, EXPoint goal, EXPoint* outPath, int32_t maxPathLen);
+
+EXTERN int32_t ex_find_path_multi_goal(const uint8_t* grid, int32_t width, int32_t height, EXPoint start, const EXPoint* goals, int32_t goalCount, EXPoint* outPath, int32_t maxPathLen);
